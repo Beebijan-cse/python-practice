e89777c28162589076883c9366903304979efeda
Beebijan-cse <beebijan10@gmail.com>
2026-05-30 15:30:08 +0530

    Create list_comprehension_examples.py
    Merged list comprehension examples into one file

---
+++ list_comprehension_examples.py
@@ -0,0 +1,22 @@
+""" list comprehension examples
+"""
+# example 1
+print([i*2 for i  in range(2,11)])
+# example 2
+print([i for i in range(20) if i %2==0])
+# example 3
+print([i**3 for i in range(1,6)])
+# example 4
+print([i for i in range(1,6)])
+# example 5
+print([i**2 for i in range(1,6)])
+# example 6
+print([i for i in range(1,11) if i%2==0])
+# example 7
+print([i**2  for i in range(1,11) if i %2==0])
+# example 8
+l=[2,5,8,11]
+print([i for i in l if i>5])
+
+
+
